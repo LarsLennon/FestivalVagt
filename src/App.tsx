25ed29c5c1b6785a07d07fc3d1637d675f6b4d47
Lars Jensen <L@rslennon.dk>
2023-02-18 23:32:54 +0100

    NavMenu removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
-import NavMenu from "./components/navbar/NavMenu";
+//import NavMenu from "./components/navbar/NavMenu";
 
 
 export default function App() {
