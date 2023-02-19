111ab3f682b2c092a88de908d50fe5bc6a43ccde
Lars Jensen <L@rslennon.dk>
2023-02-19 15:23:56 +0100

    GlobalContextProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import NavMenu from "./components/navbar/NavMenu";
+import { GlobalContextProvider } from "./hooks/GlobalContent";
 
 
 export default function App() {
   return (
-    //  <GlobalContextProvider>
-
-    <BrowserRouter>
-      <NavMenu></NavMenu>
-    </BrowserRouter>
-    //  </GlobalContextProvider>
+    <GlobalContextProvider>
+      <BrowserRouter>
+        <NavMenu></NavMenu>
+      </BrowserRouter>
+    </GlobalContextProvider>
   );
 }
